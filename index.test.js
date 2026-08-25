c68e8b5d9976ec30099f7ec995e3904252559ede
Moussa Ndiaye <moussan2003@gmail.com>
2026-08-25 18:40:43 +0000

    Test pipeline CI

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const { addition } = require('./index');
+const { addition } = require("./index");
 
-test('additionne correctement deux nombres', () => {
-  expect(addition(2, 3)).toBe(7);
+test("additionne correctement deux nombres", () => {
+  expect(addition(2, 3)).toBe(5);
 });
